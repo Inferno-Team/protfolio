813b8144cff6922f4298441bb27f38419b2d0a09
infernoacm <infernoacm@gmail.com>
2023-05-08 14:06:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function scrolHeader() {
+    let header = document.getElementById('header');
+    if (this.scrollY >= 50)
+        header.classList.add('scroll-header');
+    else
+        header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrolHeader);
