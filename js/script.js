28fa2e0dce2de3a2426a5667e3d170983c60e5d3
infernoacm <infernoacm@gmail.com>
2023-05-11 18:40:05 +0300

    contact me

--- js/script.js
+++ js/script.js
@@ -6,3 +6,46 @@ function scrolHeader() {
         header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrolHeader);
+
+/* MIX IT UP */
+
+let mixer = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* link active work */
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'));
+    this.classList.add('active-work');
+}
+linkWork.forEach(l => l.addEventListener('click', activeWork));
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// LIGTH THEME DARK THEME
