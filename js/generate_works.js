f084dbff6926ec20c6ed76ef2b296dac513fe951
Mohammed Alissa <infernoacm@gmail.com>
2024-05-29 23:55:41 +0300

    update cv info and works

--- js/generate_works.js
+++ js/generate_works.js
@@ -1,67 +1,93 @@
 export function generate_works() {
-    let works = [{
-            type: 'mobile',
-            title: 'My Day Application',
-            img: 'imgs/myday.jpg',
-            link: 'https://team-inferno-mobile-my-day-app.en.uptodown.com/',
-            langs: ['java', 'kotlin', 'back4apps']
-        },
-        {
-            type: 'laravel',
-            title: 'YAMHAD Market',
-            img: 'imgs/YAMHAD.png',
-            link: '#',
-            langs: ['laravel']
-        },
-        {
-            type: 'laravel',
-            title: 'Hotel Management API',
-            img: 'imgs/hotel.webp',
-            link: 'https://hotel.inferno-team.cloud/docs/index.html',
-            langs: ['laravel']
-        },
+  let works = [
+    {
+      type: "mobile",
+      title: "My Day Application",
+      img: "imgs/myday.jpg",
+      link: "https://play.google.com/store/apps/details?id=team.inferno.mobile.my_day_app",
+      langs: ["java", "kotlin", "back4apps"],
+    },
+    {
+      type: ["flutter", "laravel", "fullstack"],
+      title: "Healthy Minders",
+      img: "imgs/favicon.png",
+      link: "https://healthy.digiworld-dev.com",
+      langs: ["dart", "flutter", "laravel", "php", "websocket"],
+    },
+    {
+      type: ["laravel", "fullstack"],
+      title: "Survey System",
+      img: "imgs/bxs-brain.svg",
+      link: "https://int.digiworld-dev.com",
+      langs: ["laravel", "php", "blade", "vue.JS", "websocket"],
+    },
+    {
+      type: "laravel",
+      title: "YAMHAD Market",
+      img: "imgs/YAMHAD.png",
+      link: "#",
+      langs: ["laravel"],
+    },
+    {
+      type: "laravel",
+      title: "Hotel Management API",
+      img: "imgs/hotel.webp",
+      link: "#",
+      langs: ["laravel"],
+    },
 
-        {
-            type: 'fullstack',
-            title: 'Hire Me',
-            img: 'imgs/hireme.PNG',
-            link: 'https://hireme.inferno-team.cloud/',
-            langs: ['laravel', 'vue.js', 'vuetify']
-        },
-    ]
-    let works_html = ``
-    works.forEach(work => works_html += generate_work_card(work) + '\n');
-    let works_el = document.getElementById('works')
-    works_el.innerHTML = works_html
+    {
+      type: "fullstack",
+      title: "Hire Me",
+      img: "imgs/hireme.PNG",
+      link: "https://hireme.inferno-team.cloud/",
+      langs: ["laravel", "vue.js", "vuetify"],
+    },
+  ];
+  let works_html = ``;
+  console.log(Array.isArray(works[1].type));
+  works.forEach((work) => (works_html += generate_work_card(work) + "\n"));
+  let works_el = document.getElementById("works");
+  works_el.innerHTML = works_html;
 
-    /* MIX IT UP */
+  /* MIX IT UP */
 
-    let mixer = mixitup('.work__container', {
-        selectors: {
-            target: '.work__card'
-        },
-        animation: {
-            duration: 300
-        }
-    });
+  let mixer = mixitup(".work__container", {
+    selectors: {
+      target: ".work__card",
+    },
+    animation: {
+      duration: 300,
+    },
+  });
 
-    /* link active work */
-    const linkWork = document.querySelectorAll('.work__item');
+  /* link active work */
+  const linkWork = document.querySelectorAll(".work__item");
 
-    function activeWork() {
-        linkWork.forEach(l => l.classList.remove('active-work'));
-        this.classList.add('active-work');
-    }
-    linkWork.forEach(l => l.addEventListener('click', activeWork));
-
+  function activeWork() {
+    linkWork.forEach((l) => l.classList.remove("active-work"));
+    this.classList.add("active-work");
+  }
+  linkWork.forEach((l) => l.addEventListener("click", activeWork));
 }
 
 const generate_work_card = (work) => `
-         <div class="work__card mix ${work.type}">
-            <img src="${work.img}" alt="" class="work__img">
+         <div class="work__card mix ${
+           Array.isArray(work.type)
+             ? generate_type_from_array(work.type)
+             : work.type
+         }">
+            <img src="${work.img}" alt="${work.title} logo" class="work__img">
             <h3 class="work__title">${work.title}</h3>
             <a href="${work.link}" target='_blank' class="work__button">
                 Demo <i class="bx bx-right-arrow-alt work__icon"></i>
             </a>
         </div>
-`
+`;
+function generate_type_from_array(types) {
+  let string = "";
+  types.forEach((element) => {
+    string += ` ${element}`;
+  });
+  return string;
+}
--- php/send_email.php
+++ php/send_email.php
@@ -5,40 +5,32 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 function send_mail($email, $recipient_name, $message = '')
 {
-    // require("phpmailer/class.phpmailer.php");
 
+    $env = parse_ini_file(".env");
     $mail = new PHPMailer(false);
 
     $mail->CharSet = "utf-8";
-    $mail->IsSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = "smtp.ionos.com";  // Specify main and backup server
+    $mail->IsSMTP(); // Set mailer to use SMTP
+    $mail->Host = "smtp.gmail.com";  // Specify main and backup server
     $mail->SMTPAuth = true;     // Turn on SMTP authentication
-    $mail->Username = "admin@inferno-team.cloud";  // SMTP username
-    $mail->Password = "inferno-team"; // SMTP password
+    $mail->Username =  $env["Username"];  // SMTP username
+    $mail->Password = $env["Password"]; // SMTP password
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
-    $mail->setFrom('project@inferno-team.cloud', 'Project -Protfolio');
-    // $mail->FromName = "Test-Email";
+    $mail->setFrom($env["Username"], 'Project -Protfolio');
     $mail->addAddress($email, $recipient_name);
-    // $mail->addCC('admin@inferno-team.cloud');
 
     $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
     $mail->IsHTML(true);                                  // Set email format to HTML (true) or plain text (false)
 
     $mail->Subject = "New Project Idea";
     $mail->Body    = $message;
-    // $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
-    // $mail->AddEmbeddedImage('images/logo.png', 'logo', 'logo.png');
-    // $mail->addAttachment('files/file.xlsx');
 
     if (!$mail->Send()) {
-        // echo "Message could not be sent. <p>";
-        // echo "Mailer Error: " . $mail->ErrorInfo;
-        return json_encode(['result' => "Message could not be sent."]);
+        return json_encode(['result' => "Message could not be sent.",'reason' =>  $mail->ErrorInfo]);
         exit;
     }
     return json_encode(['result' => "Message has been sent"]);
-    // echo "Message has been sent";
 }
 if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['project'])) {
     echo json_encode(['result' => "All field is required."]);
@@ -48,6 +40,6 @@ $email = $_POST['email'];
 $name = $_POST['name'];
 $project = $_POST['project'];
 $msg = "Hi, New Project Idea From $name email : $email <br>$project";
-echo send_mail("project@inferno-team.cloud", 'project@inferno-team.cloud', $msg);
+echo send_mail($email, $env["Username"], $msg);
 send_mail($email, $name, 'Thank you for your idea <br>We will be in touch soon.');
 exit;
